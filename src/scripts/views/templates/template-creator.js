2f4cad27e50a54e55000363583bd22af8ed755f5
warmoa <moawarman@gmail.com>
2020-10-04 19:16:56 +0700

    foreach detail

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -9,7 +9,7 @@ const createCatalogTemplate = ({
             <figure>
                 <img src="${CONFIG.BASE_IMAGE_URL + pictureId}" class="featured-image" alt="${name}" crossorigin="anonymous">
                 <figcaption>
-                    Rating: ${rating} <br> 
+                    Rating: ${rating} <br>
                     <a href="${`/#/detail/${id}`}">${name}</a></figcaption>
             </figure>
         </div>
@@ -19,14 +19,37 @@ const createCatalogTemplate = ({
 
 const createRestaurantDetailTemplate = ({
   name, pictureId, rating, city, address, categories, menus, consumerReviews,
-}) => `
+}) => {
+    let listFood = ``;
+    let listDrink = ``;
+    let listReview = ``;
+
+    for (let food of menus.foods){
+        listFood +=`<li>${food.name}</li>`
+    };
+
+    for (let drink of menus.drinks){
+        listDrink+=`<li>${drink.name}</li>`
+    };
+
+    for (let consumerReview of consumerReviews){
+        listReview+=`
+        <div>
+            <p>${consumerReview.name}</p>
+            <p>${consumerReview.review}</p>
+            <p>${consumerReview.date}</p>
+        </div>
+         `
+    }
+
+    return (`
     <div class="catalog info">
         <div class="container">
             <div class="city">${city}</div>
             <figure>
                 <img src="${CONFIG.BASE_IMAGE_URL + pictureId}" class="featured-image" alt="${name}" crossorigin="anonymous">
                 <figcaption>
-                    Rating: ${rating} <br> 
+                    Rating: ${rating} <br>
                     ${name}</figcaption>
             </figure>
             <h3>Information</h3>
@@ -38,18 +61,16 @@ const createRestaurantDetailTemplate = ({
     </div>
     <div class="menus">
         <h4>Foods</h4>
-        <p>${menus.foods.map((food) => food.name)}</p>
+         <ul>${listFood}</ul>
         <h4>Beverages</h4>
-        <p>${menus.drinks.map((drink) => drink.name)}</p>
+        <ul>${listDrink}</ul>
     </div>
     <div class="customer__reviews">
         <h4>Reviews</h4>
-        <p>${consumerReviews[0].name}</p>
-        <p>${consumerReviews[0].review}</p>
-        <p>${consumerReviews[0].date}</p>
+        ${listReview}
     </div>
   </div>
-`;
+`);}
 
 const createFavoriteButtonTemplate = () => `
   <button aria-label="save restaurant as favorite" id="favButton" class="favorite">
